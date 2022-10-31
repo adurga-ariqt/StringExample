ac586c2cd2913ae0a17a9869c9d1b23f195c1f54
adurga-ariqt <adurga@ariqt.com>
2022-10-31 22:24:36 +0530

    Add project files.

---
+++ StringExample/StringOwnProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExample
+{
+    internal class StringOwnProgram
+    {
+        static void  Method1()
+        {
+            string sname = "durga";
+            string pname = "Bhavani";
+            string name = sname + pname;
+            Console.WriteLine(name + " " + "/nlength of the string :" + name.Length + " " + name[0]);
+        }
+        static void Method2(string fname = "durga", string zname = "bhavani")
+        {
+            Console.WriteLine(fname + zname + "Anaparthi");
+        
+        }
+        static void Main(string[] args)
+        {
+            Method1();
+            Method2( "jenny","chinni");
+            Method2(zname:"nikhil");
+            Console.ReadLine();
+        }
+    }
+}
